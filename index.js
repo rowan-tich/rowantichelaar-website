35a3027c48cd15882a56cd63fc1748f94e5f41ac
rowan-tich <rowantichelaar@gmail.com>
2024-01-08 22:51:58 -0600

    Additions
    Begun adding javascript and more detailed animations. Working on a dropdown menu.

---
+++ index.js
@@ -0,0 +1,10 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+const hiddenElemens = document.querySelectorAll(".hidden");
+hiddenElemens.forEach((el) => observer.observe(el));
