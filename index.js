9020353008bf0d080834eb8fe72d6b7e42fa96ba
rowan-tich <rowantichelaar@gmail.com>
2024-01-14 13:10:28 -0600

    Working on tech stack
    Tech stack animation done, need to fill in with content and clean up a bit.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 //Magical appearing content
-
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if(entry.isIntersecting){
@@ -11,6 +10,8 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElemens = document.querySelectorAll(".hidden");
 hiddenElemens.forEach((el) => observer.observe(el));
 
+
+//Magical menu
 function openSB() {
     document.getElementById("sb").style.display = "block";
     document.getElementById("sbopen").style.visibility = "hidden";
@@ -19,3 +20,16 @@ function closeSB() {
     document.getElementById("sb").style.display = "none";
     document.getElementById("sbopen").style.visibility = "visible";
 }
+
+
+//Magical appearing logos
+const observer2 = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add("showLogos");
+        }
+    });
+});
+
+const hiddenElemens2 = document.querySelectorAll(".hiddenLogos");
+hiddenElemens2.forEach((el) => observer2.observe(el));
